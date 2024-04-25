bea00d397fad1eaccb3bac447476674a63b5ec9e
Mehedi Hasan Joy <mmehedihasanjoyv@gmail.com>
2024-04-26 01:59:23 +0600

    created the user authentication

---
+++ src/app/(withDashboard)/dashboard/page.tsx
@@ -0,0 +1,27 @@
+import { authOptions } from "@/utils/authOptions";
+import { getServerSession } from "next-auth";
+import Image from "next/image";
+
+const DashboardPage =async () => {
+  const session=await getServerSession(authOptions)
+  return (
+    <div>
+      {
+       session&& session.user &&
+        <>
+        <h1 className="text-4xl text-center mt-10">Welcome {session?.user?.name}</h1>
+      <h1 className="text-4xl text-center mt-10">Logged-in {session?.user?.email}</h1>
+      <Image
+      src={session?.user?.image!}
+      alt="user image"
+        width={100}
+        height={100}
+      
+      />
+        </>
+      }
+    </div>
+  );
+};
+
+export default DashboardPage;
